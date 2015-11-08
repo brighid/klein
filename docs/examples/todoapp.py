b1801fd4d786ca82cbe19f94164c4c7914f551f0
Brighid M <brighid@stronglyemergent.com>
2015-11-07 21:51:23 -0800

    Clean up lint errors, add some docstrings

--- docs/examples/todoapp.py
+++ docs/examples/todoapp.py
@@ -1,11 +1,10 @@
-from os import environ
 from json import dumps, loads
+from os import environ
 from uuid import uuid4
 
 from collections import OrderedDict
-from klein import Klein
 from exceptions import Exception
-
+from klein import Klein
 from pprint import PrettyPrinter
 
 pp = PrettyPrinter(indent=4).pprint
@@ -14,11 +13,19 @@ tier = environ.get('tier', 'production')
 
 
 def parse_error(failure):
+    """
+    An error handler that turns a Python exception object into a JSON string
+    that describes the error.
+    """
     error = str(failure.type)
-    return dumps(
-        dict(error='.'.join(error[error.find("'"):error.rfind("'")].split('.')[1:]),
-             error_message=failure.getErrorMessage(),
-             **dict(stack=failure.getTraceback()) if tier == 'debug' else {}))
+    error_content = '.'.join(
+        error[error.find("'"):error.rfind("'")].split('.')[1:])
+    error_message = failure.getErrorMessage()
+    error_stack = failure.getTraceback() if tier == 'debug' else {}
+
+    return dumps({'error': error_content,
+                  'error_message': error_message,
+                  'stack': error_stack})
 
 
 class HTTPError(Exception):
@@ -46,7 +53,9 @@ class CustomerStore(object):
     app = Klein()
 
     def __init__(self):
-        self.customers = OrderedDict(dict())  # Trivial to swap out for some simple k/v store or SimpleDB
+        # This would be a good place to connect to a database that stores
+        # information about customers.
+        self.customers = OrderedDict(dict())
 
     @app.route('/', methods=['GET', 'POST'])
     def create_customer(self, request):
@@ -54,37 +63,55 @@ class CustomerStore(object):
 
         if request.method == 'POST':
             body = loads(request.content.read())
+
             if body.keys() != ['first_name', 'last_name', 'country']:
-                raise ValidationError('You must include `first_name` and `last_name` keys')
-            pk = '{first_name} {last_name}'.format(first_name=body['first_name'], last_name=body['last_name'])
+                raise ValidationError(
+                    'You must include `first_name` and `last_name` keys')
+            # It's always a good day to re-read
+            # http://www.kalzumeus.com/2010/06/17/falsehoods-programmers-believe-about-names/
+            pk = '{first_name} {last_name}'.format(
+                first_name=body['first_name'],
+                last_name=body['last_name'])
+
             if pk in self.customers:
-                raise UniqueKeyError('First name + last_name combination must be unique')
+                raise UniqueKeyError(
+                    'First name + last_name combination must be unique')
+
             body['id'] = uuid4().get_hex()
             self.customers[pk] = body
             return dumps({'created': body})
+
         else:
             if not request.args or 'limit' in request.args:
                 if not self.customers:
                     raise NotFoundError('No customers in collection')
-                return dumps(self.customers[:request.args.get('limit', 2)])  # Show only two records by default
-            elif request.args.keys() != ['first_name', 'last_name']:
-                raise ValidationError('You must include `first_name` and `last_name` keys')
-            pk = '{first_name} {last_name}'.format(first_name=request.args['first_name'][0],
-                                                   last_name=request.args['last_name'][0])
-            if pk not in self.customers:
-                raise NotFoundError('First name + last_name combination not found in collection')
+                # Show only two records by default
+                return dumps(self.customers[:request.args.get('limit', 2)])
 
+            elif request.args.keys() != ['first_name', 'last_name']:
+                raise ValidationError(
+                    'You must include `first_name` and `last_name` keys')
+            pk = '{first_name} {last_name}'.format(
+                first_name=request.args['first_name'][0],
+                last_name=request.args['last_name'][0])
+            if pk not in self.customers:
+                raise NotFoundError(
+                    'Name {!r} not found in collection'.format(pk))
             return dumps(self.customers[pk])
 
     @app.route('/<string:name>', methods=['PUT'])
     def save_customer(self, request, name):
         request.setHeader('Content-Type', 'application/json')
         body = loads(request.content.read())
-        # You can also edit the pk here, which might not be a good idea:
-        if {'first_name', 'last_name', 'id'}.issubset(set(body.keys())):  # Allow you to edit `id` here
-            raise ValidationError('You must include `first_name`, `last_name`, `country` and/or `id` key(s)')
+        # You can also edit the pk here, which might not be a good idea
+        # Allow you to edit `id` here
+        if {'first_name', 'last_name', 'id'}.issubset(set(body.keys())):
+            raise ValidationError(
+                ('You must include `first_name`, `last_name`, '
+                 '`country` and/or `id` key(s).'))
         if name not in self.customers:
-            raise NotFoundError('"{name}" not found in customers collection'.format(name=name))
+            raise NotFoundError(
+                '"{name}" not found in customer collection.'.format(name=name))
 
         self.customers[name].update(body)
         return dumps(self.customers[name])
@@ -93,14 +120,16 @@ class CustomerStore(object):
     def retrieve_customer(self, request, name):
         request.setHeader('Content-Type', 'application/json')
         if name not in self.customers:
-            raise NotFoundError('"{name}" not found in customers collection'.format(name=name))
+            raise NotFoundError(
+                '"{name}" not found in customer collection.'.format(name=name))
         return dumps(self.customers[name])
 
     @app.route('/<string:name>', methods=['DELETE'])
     def delete_customer(self, request, name):
         request.setHeader('Content-Type', 'application/json')
         if name not in self.customers:
-            raise NotFoundError('"{name}" not found in customers collection'.format(name=name))
+            raise NotFoundError(
+                '"{name}" not found in customer collection.'.format(name=name))
         return dumps({'deleted': self.customers.pop(name)})
 
     @app.handle_errors(HTTPError)
